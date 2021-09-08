5f408f6986918dc6a1b1954d3a1c0e3c337d3446
Md. Baker Hossen <76157472+bakernpp@users.noreply.github.com>
2021-09-08 16:20:40 +0600

    Add files via upload

---
+++ 494 - Kindergarten Counting Game.cpp
@@ -0,0 +1,28 @@
+    #include<bits/stdc++.h>
+    #include<cstdio>
+    #include<cstring>
+    using namespace std;
+    int main(){
+
+        int wordcnt, k;
+        char s[1003], ch;
+        while(gets(s)){
+            wordcnt = k = 0;
+            for(int i = 0; s[i]!='\0'; i++){
+                ch = s[i];
+                if(((ch >='a' && ch<='z') || (ch>='A' && ch <= 'Z')))
+                    k++;
+                else if(ch == ' ' || s[i+1] == '\0' || ch == '\\' || ch == '//' || ch == '-'){
+                    if(k>0)
+                        wordcnt++;
+                    k = 0;
+                }
+            }
+            printf("%d\n",wordcnt);
+
+
+        }
+        return 0;
+    }
+
+
